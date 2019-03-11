bfb8a30f7c82101919773676a0151a8b2859d513
Ulrich Hecht <uli@basicengine.org>
2019-03-11 11:53:35 +0100

    audio_hdmi: change settings to match 48 kHz

--- audio_hdmi.c
+++ audio_hdmi.c
@@ -17,12 +17,24 @@ void audio_hdmi_init(void)
 	HDMI_FC_AUDSCHNL4 = 0x86;
 	HDMI_FC_AUDSCHNL5 = 0x31;
 	HDMI_FC_AUDSCHNL6 = 0x75;
+	// Sampling frequency (unit?)
 	HDMI_FC_AUDSCHNL7 = 0x01;
-	HDMI_FC_AUDSCHNL7 = 0x00;	// yes, twice
+	HDMI_FC_AUDSCHNL7 = 0x02;	// yes, twice
 	HDMI_FC_AUDSCHNL8 = 0x02;
 
 	HDMI_AUD_CONF1 = 0x10;
-	HDMI_AUD_N1 = 0x80;
+
+	/* Table of "n" values for various sample rates:
+	   32000,		3072,		4096,
+	   44100,		4704,		6272,
+	   88200,		4704*2,		6272*2,
+	   176400,		4704*4,		6272*4,
+	   48000,		5120,		6144,
+	   96000,		5120*2,		6144*2,
+	   192000,		5120*4,		6144*4,
+	 */
+	// 48 kHz -> n = 6144
+	HDMI_AUD_N1 = 0x00;
 	HDMI_AUD_N2 = 0x18;
 	HDMI_AUD_N3 = 0x00;
 	HDMI_AUD_CTS3 = 0x00;
