b8852c49c286acc1e4a497948294d15a37c93612
Ulrich Hecht <uli@basicengine.org>
2019-03-11 14:17:20 +0100

    audio: use correct settings for 2 channels at 48 kHz

--- audio_i2s.c
+++ audio_i2s.c
@@ -8,8 +8,8 @@ int sample_count;
 void audio_queue_samples(void)
 {
 	while (((I2S_FSTA(2) >> 16) & 0xff) > 2) {
-		I2S_TXFIFO(2) = (sample_count / 100) * 0xffff;
-		I2S_TXFIFO(2) = (sample_count / 100) * 0xffff;
+		I2S_TXFIFO(2) = ((sample_count / 50) & 1) * 0x17ff;
+		I2S_TXFIFO(2) = ((sample_count / 50) & 1) * 0x17ff;
 		sample_count++;
 	}
 	// TXEI clears itself when the FIFO is full again
@@ -18,7 +18,8 @@ void audio_queue_samples(void)
 void audio_i2s2_init(void)
 {
 	// enable audio PLL, default value (24.571 MHz)
-	PLL_AUDIO_CTRL = 0x810d0d00;
+	// XXX: should be 24.576 MHz for 48 kHz
+	PLL_AUDIO_CTRL = 0x80035514;
 	while (!(PLL_AUDIO_CTRL & (1 << 28))) {
 		// wait
 	}
@@ -26,11 +27,11 @@ void audio_i2s2_init(void)
 	// enable clock, source PLL_AUDIO (no multiplier)
 	// XXX: correct?
 	I2S_PCM2_CLK = 0x80030000;
-	BUS_SOFT_RST3 &= ~(1 << 14);
+	BUS_SOFT_RST3 &= ~(1 << 14);	// assert reset
 	udelay(100);
-	BUS_SOFT_RST3 |= 1 << 14;
+	BUS_SOFT_RST3 |= 1 << 14;	// deassert reset
 	udelay(100);
-	BUS_CLK_GATING2 |= (1 << 14);
+	BUS_CLK_GATING2 |= (1 << 14);	// pass clock
 	udelay(100);
 }
 
@@ -39,9 +40,7 @@ void audio_i2s2_on(void)
 	I2S_TX0CHSEL_H3(2) = I2S_TXnCHSEL_OFFSET_1 | I2S_TXnCHSEL_CHSEL_2 |
 			     I2S_TXnCHSEL_CHEN_0 | I2S_TXnCHSEL_CHEN_1; // 0x00001031
 
-	// XXX: This is the value used by Linux, but shouldn't it be more like
-	// "I2S_TXnCHMAP_CH0_SAMPLE(0) | I2S_TXnCHMAP_CH1_SAMPLE(1)"?
-	I2S_TX0CHMAP_H3(2) = I2S_TXnCHMAP_CH0_SAMPLE(0) | I2S_TXnCHMAP_CH1_SAMPLE(0);
+	I2S_TX0CHMAP_H3(2) = I2S_TXnCHMAP_CH0_SAMPLE(0) | I2S_TXnCHMAP_CH1_SAMPLE(1);
 
 	// Set clock dividers.
 	// BCLK/8 is good for both 44.1 and 48 kHz, must set input
@@ -49,7 +48,7 @@ void audio_i2s2_on(void)
 	I2S_CLKD(2) = I2S_CLKD_MCLKDIV(1) | I2S_CLKD_BCLKDIV(5) | I2S_CLKD_MCLKO_EN; // 0x00000151
 
 	// TX/RX mode PCM, no sign extension
-	I2S_FAT1(2) = 0;
+	I2S_FAT1(2) = 0;	// 0x00000000
 
 	/* "normal bit clock + frame" (BCLK, LRCK polarity 0) */
 	// LRCK period 32 BCLKs, LRCKR period 1 BCLK
@@ -57,7 +56,7 @@ void audio_i2s2_on(void)
 	I2S_FAT0(2) = I2S_FAT0_SW(3) | I2S_FAT0_SR(3) |
 		      I2S_FAT0_LRCK_PERIOD(31) | I2S_FAT0_LRCKR_PERIOD(0);	// 0x00001F33
 
-	I2S_TXCHCFG_H3(2) = I2S_CHCFG_RX_SLOT_NUM(1) | I2S_CHCFG_TX_SLOT_NUM(0);// 0x00000010 check
+	I2S_TXCHCFG_H3(2) = I2S_CHCFG_RX_SLOT_NUM(1) | I2S_CHCFG_TX_SLOT_NUM(1);
 
 	I2S_RXCHSEL(2) = I2S_RXCHSEL_OFFSET(1) | I2S_RXCHSEL_CHSEL(1);	// 0x00001001
 	I2S_RXCHMAP(2) = I2S_RXCHMAP_CH_SAMPLE(0, 0) | I2S_RXCHMAP_CH_SAMPLE(1, 1); // 0x00000010
@@ -90,10 +89,8 @@ void audio_i2s2_off(void)
 	I2S_FCTL(2) |= I2S_FCTL_FRX;	// flush RX FIFO
 	I2S_RXCNT(2) = 0;		// clear RX count
 	// don't have to disable DRQ, we don't enable it
+	I2S_INT(2) = 0;
 
-	// disable TX
-	// don't have to disable DRQ, we don't enable it
-	// XXX: doesn't do a flush for some reason
-
+	// XXX: should we do a flush?
 	// XXX: hub enable/disable? Not enabled in register trace.
 }
