29680cb3929f149151d7c0a5f668d994a0daad92
Ulrich Hecht <uli@basicengine.org>
2019-03-13 15:56:02 +0100

    build system: add -MMD to CXXFLAGS

--- common.mk
+++ common.mk
@@ -5,8 +5,8 @@ CXX=$(PREFIX)g++
 OBJCOPY=$(PREFIX)objcopy
 AR=$(PREFIX)ar
 CFLAGS_BIN=-O3 -mfpu=neon -mfloat-abi=hard -mcpu=cortex-a7 -fpic -ffreestanding
-CFLAGS_COMMON=$(CFLAGS_BIN) -std=gnu99
-CFLAGS=-MMD -T $(OSDIR)/linker.ld $(CFLAGS_COMMON) -nostdlib -Wall -Wextra \
+CFLAGS_COMMON=$(CFLAGS_BIN) -std=gnu99 -MMD
+CFLAGS=-T $(OSDIR)/linker.ld $(CFLAGS_COMMON) -nostdlib -Wall -Wextra \
 	-I $(OSDIR) -I $(OSDIR)/tinyusb/src
 CXXFLAGS=$(CFLAGS_BIN) -nostdlib -Wall -Wextra -I $(OSDIR) -I $(OSDIR)/tinyusb/src
 
