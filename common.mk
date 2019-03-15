eda523a3e010dc0f5d0a197c60c96158ca8fd432
Ulrich Hecht <uli@basicengine.org>
2019-03-15 11:43:40 +0100

    common.mk: -std=gnu99 no longer required

--- common.mk
+++ common.mk
@@ -4,11 +4,10 @@ CC=$(PREFIX)gcc
 CXX=$(PREFIX)g++
 OBJCOPY=$(PREFIX)objcopy
 AR=$(PREFIX)ar
-CFLAGS_BIN=-MMD -O3 -mfpu=neon -mfloat-abi=hard -mcpu=cortex-a7 -fpic -ffreestanding
-CFLAGS_COMMON=$(CFLAGS_BIN) -std=gnu99
+CFLAGS_COMMON=-MMD -O3 -mfpu=neon -mfloat-abi=hard -mcpu=cortex-a7 -fpic -ffreestanding
 CFLAGS=-T $(OSDIR)/linker.ld $(CFLAGS_COMMON) -nostdlib -Wall -Wextra \
 	-I $(OSDIR) -I $(OSDIR)/tinyusb/src
-CXXFLAGS=$(CFLAGS_BIN) -nostdlib -Wall -Wextra -I $(OSDIR) -I $(OSDIR)/tinyusb/src
+CXXFLAGS=$(CFLAGS_COMMON) -nostdlib -Wall -Wextra -I $(OSDIR) -I $(OSDIR)/tinyusb/src
 
 %.o: %.s
 	$(CC) $(CFLAGS) -o $@ -c $<
