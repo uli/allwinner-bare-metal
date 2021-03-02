4eef3de63be6ff92580b1bbd9ad73512849c102f
Ulrich Hecht <uli@basicengine.org>
2021-03-02 15:26:22 +0100

    exceptions: print stack traces on undefined insn, SWI

--- exceptions.c
+++ exceptions.c
@@ -6,18 +6,33 @@ void halt(void)
 	for (;;);
 }
 
+static inline void stack_dump(uint32_t *stack)
+{
+	uart_print("Dump from "); uart_print_uint32((uint32_t)stack);
+	for(;;);
+	for (int i = 0; i < 32; ++i) {
+		uart_print("\r\nSP-");uart_print_uint8(i);
+		uart_print("  ");
+		uart_print_uint32(stack[-i]);
+	}
+	uart_print("\r\n");
+}
+
 void __attribute__((interrupt("UNDEF"))) except_undef(void)
 {
-	register uint32_t reg_lr asm("lr");
-	uint32_t saved_lr = reg_lr;
-	uart_print("undefined instruction at ");
-	uart_print_uint32(saved_lr);
+	register uint32_t *reg_sp asm("sp");
+	uint32_t *saved_sp = reg_sp;
+	uart_print("undefined instruction");
+	stack_dump(saved_sp);
 	halt();
 }
 
 void __attribute__((interrupt("SWI"))) except_swi(void)
 {
-	uart_print("SWI\r\n");
+	register uint32_t *reg_sp asm("sp");
+	uint32_t *saved_sp = reg_sp;
+	uart_print("SWI");
+	stack_dump(saved_sp);
 	halt();
 }
 
