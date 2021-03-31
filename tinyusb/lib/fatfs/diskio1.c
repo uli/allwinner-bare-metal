79eb4e1560be936ae99433c1b5c22972324cd3d2
Ulrich Hecht <uli@basicengine.org>
2021-03-31 17:47:11 +0200

    tinyusb: wrap mass-storage code

---
+++ tinyusb/lib/fatfs/diskio1.c
@@ -0,0 +1,4 @@
+#define USB 1
+#define USBP(x) usb1_ ## x
+#include "../../src/redef.h"
+#include "diskio.c"
---
+++ tinyusb/lib/fatfs/diskio2.c
@@ -0,0 +1,4 @@
+#define USB 2
+#define USBP(x) usb2_ ## x
+#include "../../src/redef.h"
+#include "diskio.c"
