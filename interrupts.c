3025ae5306227abb97a63448876d1f83fb321921
Ulrich Hecht <uli@basicengine.org>
2021-04-12 14:56:21 +0200

    interrupts: tweak format, comments (no functional changes)

--- interrupts.c
+++ interrupts.c
@@ -1,9 +1,10 @@
 #include <stdint.h>
-#include "uart.h"
-#include "interrupts.h"
-#include "display.h"
+
 #include "audio.h"
+#include "display.h"
+#include "interrupts.h"
 #include "system.h"
+#include "uart.h"
 #include "util.h"
 
 extern uint32_t _ivt;
@@ -14,7 +15,7 @@ void usb3_hal_hcd_isr(uint8_t hostid);
 
 int irq_pending(uint32_t irq)
 {
-  volatile struct gicd_reg* gicd = (volatile struct gicd_reg*) GICD_BASE;
+  volatile struct gicd_reg *gicd = (volatile struct gicd_reg *)GICD_BASE;
   if (gicd->ispendr[irq / 32] & (1 << (irq % 32)))
     return 1;
   return 0;
@@ -28,7 +29,8 @@ void __attribute__((weak)) hook_display_vblank(void)
 extern void codec_fiq_handler(void);
 
 // Called when an interrupt is triggered
-void __attribute__((interrupt("IRQ"))) interrupt(void) {
+void __attribute__((interrupt("IRQ"))) interrupt(void)
+{
   // digital audio
   if (irq_pending(47))
     audio_queue_samples();
@@ -47,15 +49,16 @@ void __attribute__((interrupt("IRQ"))) interrupt(void) {
   if (irq_pending(111))
     usb3_hal_hcd_isr(0);
 
-  // LCD controller
+  // LCD controllers 0 and 1
+  // NB: Only one of these will be enabled at any time.
   if (irq_pending(118)) {
     tick_counter++;
-    LCD0_GINT0 &= ~(1<<14);
+    LCD0_GINT0 &= ~(1 << 14);
     hook_display_vblank();
   }
   if (irq_pending(119)) {
     tick_counter++;
-    LCD1_GINT0 &= ~(1<<14);
+    LCD1_GINT0 &= ~(1 << 14);
     hook_display_vblank();
   }
 }
@@ -68,43 +71,43 @@ void __attribute__((interrupt("IRQ"))) interrupt(void) {
 
 void irq_enable(uint32_t irq)
 {
-  volatile struct gicd_reg* gicd = (volatile struct gicd_reg*) GICD_BASE;
+  volatile struct gicd_reg *gicd = (volatile struct gicd_reg *)GICD_BASE;
 #ifdef GDBSTUB
   gicd->ctlr = BIT(1) | BIT(0);
 #else
   gicd->ctlr = BIT(1);
 #endif
-  gicd->igroupr[irq/32]  |= 1UL << (irq % 32);	// set to group 1
-  gicd->isenabler[irq/32] = 1UL << (irq % 32);
-  gicd->itargetsr[irq] = 1;	// target core 0
-  gicd->ipriorityr[irq] = 2;	// lower than FIQ
-
+  gicd->igroupr[irq / 32] |= 1UL << (irq % 32);  // set to group 1
+  gicd->isenabler[irq / 32] = 1UL << (irq % 32);
+  gicd->itargetsr[irq]      = 1;  // target core 0
+  gicd->ipriorityr[irq]     = 2;  // lower than FIQ
 }
 
 void irq_enable_fiq(uint32_t irq)
 {
-  volatile struct gicd_reg* gicd = (volatile struct gicd_reg*) GICD_BASE;
+  volatile struct gicd_reg *gicd = (volatile struct gicd_reg *)GICD_BASE;
 
   irq_enable(irq);
 
-  gicd->igroupr[irq/32] &= ~(1UL << (irq % 32));	// set to group 0
-  gicd->ipriorityr[irq] = 1;	// higher than IRQ
+  gicd->igroupr[irq / 32] &= ~(1UL << (irq % 32));  // set to group 0
+  gicd->ipriorityr[irq] = 1;                        // higher than IRQ
 }
 
 void irq_disable(uint32_t irq)
 {
-  volatile struct gicd_reg* gicd = (volatile struct gicd_reg*) GICD_BASE;
-  gicd->icenabler[irq/32] = 1 << (irq % 32);
+  volatile struct gicd_reg *gicd = (volatile struct gicd_reg *)GICD_BASE;
+  gicd->icenabler[irq / 32]      = 1 << (irq % 32);
 }
 
 // Copy the interrupt table from _ivt to 0x0
-void __attribute__((no_sanitize("all"))) install_ivt() {
-  uint32_t* source = &_ivt;
-  uint32_t* destination = (uint32_t*)(0);
-  for(int n=0; n<2*8; n++)
+void __attribute__((no_sanitize("all"))) install_ivt()
+{
+  uint32_t *source      = &_ivt;
+  uint32_t *destination = (uint32_t *)(0);
+  for (int n = 0; n < 2 * 8; n++)
     destination[n] = source[n];
 
-  volatile struct gicc_reg* gicc = (volatile struct gicc_reg*) GICC_BASE;
+  volatile struct gicc_reg *gicc = (volatile struct gicc_reg *)GICC_BASE;
 #ifdef GDBSTUB
   // enable interrupt groups 0 and 1, signal group 0 as FIQ
   gicc->ctlr = BIT(3) | BIT(1) | BIT(0);
@@ -114,10 +117,10 @@ void __attribute__((no_sanitize("all"))) install_ivt() {
 #endif
   gicc->pmr = 10;
 
-  volatile struct gicd_reg* gicd = (volatile struct gicd_reg*) GICD_BASE;
+  volatile struct gicd_reg *gicd = (volatile struct gicd_reg *)GICD_BASE;
   // set all interrupts to group 1
   for (int i = 0; i < 32; ++i)
     gicd->igroupr[i] = 0xffffffff;
 
-  asm("cpsie if;"); // Enable interrupts
+  asm("cpsie if;");  // Enable interrupts
 }
