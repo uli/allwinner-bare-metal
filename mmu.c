434fcb7c6f0476ba3f105da3914ad5eca059c381
Ulrich Hecht <uli@basicengine.org>
2021-04-12 15:00:15 +0200

    mmu: format tweaks (no functional changes)

--- mmu.c
+++ mmu.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
+
+#include <arm/synchronize.h>
 #include "mmu.h"
 #include "uart.h"
-#include <arm/synchronize.h>
 
 #define DRAM_START 0x40000000
 #define DRAM_MAX   0xc0000000
@@ -10,7 +11,7 @@
 void *mmu_detect_dram_end(void)
 {
   volatile uint32_t *dram_start = (uint32_t *)DRAM_START;
-  volatile uint32_t *dram_end = dram_start + DRAM_STEP / sizeof(uint32_t);
+  volatile uint32_t *dram_end   = dram_start + DRAM_STEP / sizeof(uint32_t);
 
   uint32_t saved_dram_start = *dram_start;
   uint32_t saved_dram_end;
@@ -32,38 +33,41 @@ void *mmu_detect_dram_end(void)
   }
   *dram_start = saved_dram_start;
 
-  uart_print_uint32((uint32_t)dram_end); uart_print(" RAM limit\r\n");
+  uart_print_uint32((uint32_t)dram_end);
+  uart_print(" RAM limit\r\n");
   return (void *)dram_end;
 }
 
-void mmu_init() {
-
+void mmu_init()
+{
   // Disable MMU
   asm("ldr r8, =0x0;    mcr p15, 0, r8, c1, c0, 0;" : : : "r8");
 
   // Enable cache coherency
-  asm("mrc p15, 0, r8, c1, c0, 1;"
-      "orr r8, r8, #0x40;"
-      "mcr p15, 0, r8, c1, c0, 1;" ::: "r8");
+  asm(
+    "mrc p15, 0, r8, c1, c0, 1;"
+    "orr r8, r8, #0x40;"
+    "mcr p15, 0, r8, c1, c0, 1;" ::
+      : "r8");
 
   // Populate the pagetable
-  volatile uint32_t* pagetable = (volatile uint32_t *)0xc000;
+  volatile uint32_t *pagetable = (volatile uint32_t *)0xc000;
   for (uint32_t n = 0; n < 0x1000; n++) {
-    if(n==0) {
+    if (n == 0) {
       // SRAM.  Write back.
-      pagetable[n] = (n<<20) | (1<<12) | (3<<10) | (3<<2) | 2;
-    } else if(n>=0x400 && n<0x410) {
+      pagetable[n] = (n << 20) | (1 << 12) | (3 << 10) | (3 << 2) | 2;
+    } else if (n >= 0x400 && n < 0x410) {
       // First half of DRAM. Write back.
-      pagetable[n] = (n<<20) | (1<<12) | (3<<10) | (3<<2) | 2;
-    } else if(n>=0x410 && n<0x420) {
+      pagetable[n] = (n << 20) | (1 << 12) | (3 << 10) | (3 << 2) | 2;
+    } else if (n >= 0x410 && n < 0x420) {
       // DMA buffers and such. Normal uncached.
-      pagetable[n] = (n<<20) | (1<<12) | (3<<10) | (0<<2) | 2;
-    } else if(n>=0x420 && n<0xc00) {
+      pagetable[n] = (n << 20) | (1 << 12) | (3 << 10) | (0 << 2) | 2;
+    } else if (n >= 0x420 && n < 0xc00) {
       // Second half of DRAM. Write back.
-      pagetable[n] = (n<<20) | (1<<12) | (3<<10) | (3<<2) | 2;
+      pagetable[n] = (n << 20) | (1 << 12) | (3 << 10) | (3 << 2) | 2;
     } else {
       // Other stuff. Strictly ordered for safety.
-      pagetable[n] = (n<<20) | (0<<12) | (3<<10) | (0<<2) | 2;
+      pagetable[n] = (n << 20) | (0 << 12) | (3 << 10) | (0 << 2) | 2;
     }
   }
 
@@ -90,6 +94,7 @@ void mmu_init() {
 
     "ldr r8, =0x1005;"
     "mcr p15, 0, r8, c1, c0, 0;"
-    : : : "r8");
-
+    :
+    :
+    : "r8");
 }
