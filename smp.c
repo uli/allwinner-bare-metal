e7a77b7abfaa5e8c51bbe909d42f5dbf434a0cc2
Ulrich Hecht <uli@basicengine.org>
2021-04-12 14:35:00 +0200

    smp: format tweaks (no functional changes)

--- smp.c
+++ smp.c
@@ -2,37 +2,38 @@
 // Copyright (c) 2021 Ulrich Hecht
 
 #include <stdint.h>
+
 #include "mmu.h"
 #include "smp.h"
 
 #define CPU_REG(n) (*(volatile uint32_t *)(0x01f01c00 + (n)))
 
-#define CPU_RST_CTRL_REG(n)	CPU_REG(0x40 + (n) * 0x40)
-#define CPU_CTRL_REG(n)		CPU_REG(0x44 + (n) * 0x40)
-#define CPU_STATUS_REG(n)	CPU_REG(0x48 + (n) * 0x40)
+#define CPU_RST_CTRL_REG(n) CPU_REG(0x40 + (n)*0x40)
+#define CPU_CTRL_REG(n)     CPU_REG(0x44 + (n)*0x40)
+#define CPU_STATUS_REG(n)   CPU_REG(0x48 + (n)*0x40)
 
 extern volatile uint32_t _smp_secondary_sp;
 volatile secondary_task_t tasks[4] = {};
 
-void smp_start_secondary_core(int cpuid, secondary_task_t task, void *stack,
-                              uint32_t stack_size)
+void smp_start_secondary_core(int cpuid, secondary_task_t task, void *stack, uint32_t stack_size)
 {
-    tasks[cpuid] = task;
-    _smp_secondary_sp = (uint32_t)stack + stack_size - 0x400;
-    CPU_RST_CTRL_REG(cpuid) = 0x3;
+  tasks[cpuid]            = task;
+  _smp_secondary_sp       = (uint32_t)stack + stack_size - 0x400;
+  CPU_RST_CTRL_REG(cpuid) = 0x3;
 }
 
 void smp_stop_secondary_core(int cpuid)
 {
-    CPU_RST_CTRL_REG(cpuid) = 0;
+  CPU_RST_CTRL_REG(cpuid) = 0;
 }
 
 void init_sp_irq(uint32_t addr);
 
-#include "uart.h"
 #include "system.h"
+#include "uart.h"
 
-void _smp_startup_secondary(int cpuid) {
+void _smp_startup_secondary(int cpuid)
+{
   init_sp_irq(_smp_secondary_sp + 0x400);
   mmu_init();
   uart_print("Secondary boot\r\n");
@@ -41,4 +42,3 @@ void _smp_startup_secondary(int cpuid) {
       tasks[cpuid]();
   }
 }
-
