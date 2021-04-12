a3289362522b583714d35e924b81d6473abc3309
Ulrich Hecht <uli@basicengine.org>
2021-04-12 15:04:49 +0200

    system: format tweaks (no functional changes)

--- system.c
+++ system.c
@@ -1,38 +1,41 @@
-#include "system.h"
 #include <stdio.h>
+#include "system.h"
 
 static uint32_t sys_freq;
 static uint32_t sys_per_usec;
 
 // used before the stack guard is set up
-void __attribute__((optimize("-fno-stack-protector"))) init_bss() {
-  for (char* dst = &_bstart1; dst < &_bend1; dst++)
+void __attribute__((optimize("-fno-stack-protector"))) init_bss()
+{
+  for (char *dst = &_bstart1; dst < &_bend1; dst++)
     *dst = 0;
-  for (char* dst = &_bstart2; dst < &_bend2; dst++)
+  for (char *dst = &_bstart2; dst < &_bend2; dst++)
     *dst = 0;
 }
 
-void udelay(uint32_t d) {
-  for(uint32_t n=0;n<d*200;n++) asm("NOP");
+void udelay(uint32_t d)
+{
+  for (uint32_t n = 0; n < d * 200; n++)
+    asm("NOP");
 }
 
 uint64_t sys_get_tick(void)
 {
-	uint32_t low, high;
-	uint64_t tick;
-	asm volatile("mrrc p15, 0, %0, %1, c14" : "=r" (low), "=r" (high));
-	tick = ((uint64_t)high << 32) | low;
-	return tick;
+  uint32_t low, high;
+  uint64_t tick;
+  asm volatile("mrrc p15, 0, %0, %1, c14" : "=r"(low), "=r"(high));
+  tick = ((uint64_t)high << 32) | low;
+  return tick;
 }
 
 uint64_t sys_get_usec(void)
 {
-	return sys_get_tick() / sys_per_usec;
+  return sys_get_tick() / sys_per_usec;
 }
 
 uint64_t sys_get_msec(void)
 {
-	return sys_get_tick() / sys_per_usec / 1000;
+  return sys_get_tick() / sys_per_usec / 1000;
 }
 
 void sys_init_timer(void)
@@ -52,7 +55,7 @@ void sys_init_timer(void)
 
   t2 = sys_get_tick();
 
-  sys_freq = (t2 - t1) * 60;
+  sys_freq     = (t2 - t1) * 60;
   sys_per_usec = sys_freq / 1000000;
   printf("freq %lu per usec %lu\n", sys_freq, sys_per_usec);
 }
