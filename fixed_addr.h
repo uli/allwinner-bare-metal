8cc87fc22eda7aa2ce8a2e14c0d5f68cf300bfb9
Ulrich Hecht <uli@basicengine.org>
2023-05-29 13:35:59 +0200

    fixed_addr.h: define communication and coherent buffers areas

--- fixed_addr.h
+++ fixed_addr.h
@@ -2,11 +2,16 @@
 
 #ifdef JAILHOUSE
 
+// start of application memory
 #define AWBM_BASE_ADDR		0x49000000
-#define SDL_EVENT_BUFFER_ADDR	0x488f1000
-#define LIBC_CALL_BUFFER_ADDR	0x488fc000
-#define GDBSTUB_PORT_ADDR	0x488fa000
 
+// Jailhouse root cell communication shared memory areas
+#define JAILHOUSE_COMM_BASE	0x488f0000
+#define SDL_EVENT_BUFFER_ADDR	(JAILHOUSE_COMM_BASE + 0x1000)
+#define LIBC_CALL_BUFFER_ADDR	(JAILHOUSE_COMM_BASE + 0xc000)
+#define GDBSTUB_PORT_ADDR	(JAILHOUSE_COMM_BASE + 0xa000)
+
+// application exception vectors
 #define AWBM_IRQ_HANDLER_VECTOR	0xfffc
 #define GDBSTUB_IRQ_HANDLER_VECTOR 0xfff8
 #define GDBSTUB_SVC_HANDLER_VECTOR 0xfff4
@@ -14,6 +19,10 @@
 
 #else
 
+// start of application memory
 #define AWBM_BASE_ADDR		0x40000000
 
 #endif
+
+// uncached memory area for DMA buffers
+#define AWBM_UNCACHED_ADDR	(AWBM_BASE_ADDR + 0x01000000)
