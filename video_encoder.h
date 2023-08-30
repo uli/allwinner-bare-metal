e7b1120dd922115d8e16635bfa9b2fb40731e55e
Ulrich Hecht <uli@basicengine.org>
2023-08-30 13:13:13 +0200

    add video encoder interface headers

---
+++ video_encoder.h
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2023 Ulrich Hecht
+
+// Interface to H.264 encoder process on the Linux side.
+
+#include "fixed_addr.h"
+
+struct video_encoder_comm_buffer {
+    void *video_luma;
+    void *video_chroma;
+    int video_frame_no;
+    int video_w;
+    int video_h;
+    void *audio_buffer;
+    int audio_frame_no;
+    int enabled;
+};
+
+static volatile struct video_encoder_comm_buffer *video_encoder =
+    (volatile struct video_encoder_comm_buffer *)VIDEO_ENCODER_PORT_ADDR;
