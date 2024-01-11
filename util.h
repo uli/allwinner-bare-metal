63d46e60c55053f50487185599ad4e5b210fcce7
Ulrich Hecht <uli@basicengine.org>
2024-01-11 07:24:35 +0100

    util.h: add MMIO macros

--- util.h
+++ util.h
@@ -1,6 +1,11 @@
 #ifndef _UTIL_H
 #define _UTIL_H
 
+#include <stdint.h>
+
 #define BIT(n) (1UL << (n))
+
+#define MEM(a) (*(volatile uint32_t *)(a))
+#define MEM8(a) (*(volatile uint8_t *)(a))
 
 #endif
